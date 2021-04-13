d2dc22f9c34007d881aa9e854709c2c295cfcac5
Ranjodh <34418817+Ranjodh86@users.noreply.github.com>
2021-04-13 16:12:19 +0530

    Update environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -6,13 +6,7 @@ export const environment = {
   production: false,
   appName: 'WTP',
   firebase: {
-    apiKey: "AIzaSyBy512H4VrwbcrKdn1388HZn5HAV9EH6H0",
-    authDomain: "test-e701a.firebaseapp.com",
-    projectId: "test-e701a",
-    storageBucket: "test-e701a.appspot.com",
-    messagingSenderId: "671393538913",
-    appId: "1:671393538913:web:0a0611c0dfaf2aed021b20",
-    measurementId: "G-C8ESBBSDBQ"
+    
   }
   };
 
